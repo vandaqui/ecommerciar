9993d66d1d0c59b5a0562f0ebb9952f02d6f0d1e
Vand Aquino <vandicleydson@gmail.com>
2021-11-13 14:36:19 -0300

    home sec

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ $(document).ready(function(){
 
     $('.fa-bars').click(function (){
         $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
       });
 
-
+      $(window).on('load scroll', function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+      });
 });
