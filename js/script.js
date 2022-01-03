deff2ee61be5647734869cca02d5762f270405e8
Vand <vandicleydson@gmail.com>
2022-01-03 11:47:52 -0300

    Add scrolling code

--- js/script.js
+++ js/script.js
@@ -22,5 +22,21 @@ $(document).ready(function(){
         $('.accordion .accordion-heading span').text('+');
         $(this).children('span').text('-');
       });
+});
 
+/* Scroll*/
+$(document).on('click', 'a[href^="#"]', function(e) {
+  // target element id
+  var id = $(this).attr('href');
+  // target element
+  var $id = $(id);
+  if ($id.length === 0) {
+      return;
+  }
+  // prevent standard hash navigation (avoid blinking in IE)
+  e.preventDefault();
+  // top position relative to the document
+  var pos = $id.offset().top;
+  // animated top scrolling
+  $('body, html').animate({scrollTop: pos});
 });
