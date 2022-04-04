0140928173b4e1f4eaf237067411c5bfe1664333
vandicleydson <vandicleydson@gmail.com>
2022-04-04 15:10:59 -0300

    CSS Update

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
         $('.fa-bars').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
 
-        if($(window).scrollTop() > 30){
+        if($(window).scrollTop() > 10){
             $('.top-nav').css({'background':'#FFC727','box-shadow':'0 .2rem 5.rem rgba(0,0,0,.4)','z-index': '100'});
         } else {
             $('.top-nav').css({'background':'none','box-shadow':'none'});
